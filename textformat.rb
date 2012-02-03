bf1ba7a61669622dda8d4a0d8ef089aea52f9757
luchmhor <gaelic@luchmhor.net>
2012-02-03 20:45:46 +0100

    updated textformat

--- textformat.rb
+++ textformat.rb
@@ -6,33 +6,32 @@ def multiplication_table(number, heading = '', decorate = false)
   table = ''
   num_length = (number*number).to_s.length
   row_length = number*(num_length+1)
-  if heading != ''
+  unless heading.empty?
     header_length = heading.length
-    adjust_header = (row_length-header_length)/2
+    adjust_header = 0||((row_length-header_length)/2 if (row_length > header_length))
     table << " "*adjust_header+"#{heading}\n"
   end
-  if decorate
-    table << deco(row_length)
-  end
+    table << deco(row_length) if decorate
   (1..number).each do |col|
     (1..number).each do |row|
       table << " %#{num_length}d" %(col*row)
     end
     table << "\n"
   end
-  if decorate
-    table << deco(row_length)
-  end
-  return table
+    table << deco(row_length) if decorate
+  table
 end
 
 def deco(length)
-  return " "+"="*length+"\n"
+  " "+"="*length+"\n"
 end
 
-tab1 = multiplication_table(9, 'Times Table to 9', true)
-tab2 = multiplication_table(20, '', false)
+tab1 = multiplication_table(0, 'Times Table to 0', true)
+tab2 = multiplication_table(1, 'Times Table to 1', true)
+tab3 = multiplication_table(20, '', false)
 
 puts tab1
 puts
 puts tab2
+puts
+puts tab3
