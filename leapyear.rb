210878608ec9ea66e3eec15922cd9fbecc0bf133
luchmhor <gaelic@luchmhor.net>
2012-02-09 12:18:18 +0100

    update ?

--- leapyear.rb
+++ leapyear.rb
@@ -1,9 +1,9 @@
-def leap_year(year)
+def leap_year?(year)
   year%100 == 0 ? (year%400 ==0) : (year%4 == 0)
 end
 
 year = 2003
 
-minutes = leap_year(year) ? (366*24*60) : (365*24*60)
+minutes = leap_year?(year) ? (366*24*60) : (365*24*60)
 
 puts "Year #{year} has #{minutes} minutes\n"
