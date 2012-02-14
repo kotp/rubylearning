7ca43b19c6ae1e26d0321242fbab8b6166a5bf91
luchmhor <gaelic@luchmhor.net>
2012-02-14 16:50:59 +0100

    grandma update

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,11 @@
-loop {
+while :true do
   print "You enter: "
   input = gets.chomp
   break if input.eql?('BYE')
   if input.eql?(input.upcase)
-    year = 1930+rand(21).to_i
+    year = 1930+rand(21)
     puts "Grandma responds: NO, NOT SINCE #{year}!"
   else
     puts "Grandma responds: HUH?!SPEAK UP, SONNY!"
   end
-}
+end
