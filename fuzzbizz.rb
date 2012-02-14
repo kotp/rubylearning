0c3a8465f967154e535418d97f5667f4f3608651
luchmhor <gaelic@luchmhor.net>
2012-02-14 17:38:24 +0100

    rewrote with ternary

--- fuzzbizz.rb
+++ fuzzbizz.rb
@@ -1,11 +1,3 @@
-100.times do |i|
-  if ((i+1)%3 == 0 && (i+1)%5 ==0)
-    puts "FuzzBizz"
-  elsif (i+1)%5 == 0
-    puts "Fuzz"
-  elsif (i+1)%3 == 0
-    puts "Bizz"
-  else
-    puts i+1
-  end
+1.upto(100) do |i|
+  puts i%3 == 0 ? i%5 == 0 ? "FizzBuzz" : "Fizz" : i%5 ==0 ? "Buzz" : i
 end
